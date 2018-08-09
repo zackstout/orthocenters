7c87efe810327f5c765c2065a7ffd6da2e4cb035
Zack Stout <zackstout@gmail.com>
2018-08-09 00:06:38 -0500

    orthogonal lines meeting now

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# Orthogonal Circles
+Practicing building an interactive user interface, where user can click and drag on vertices to change the image that is drawn.
--- scripts/draw.js
+++ scripts/draw.js
@@ -1,9 +1,12 @@
 
 const vertices = [{x: 100, y: 100}, {x: 300, y: 600}, {x: 700, y: 400}]; // will making them objects create a mutability problem?
-const mouseSens = 5;
+const mouseSens = 9;
 let grabbing_staged = false;
 let grabbing = false;
 let grabbed_vtx;
+let orthos = [];
+
+// expected mdpts: (200, 350), (400, 250), (500, 500) -- It works!
 
 function setup() {
   createCanvas(800, 800);
@@ -15,6 +18,9 @@ function setup() {
 function draw() {
   background(220);
   drawTriangle();
+  orthos.forEach(o => {
+    drawLine(o.m, o.p);
+  });
 }
 
 
@@ -34,12 +40,22 @@ function drawTriangle() {
 
 function mouseMoved() {
   // console.log(mouseX, mouseY);
-  vertices.forEach(v => {
+  orthos = [];
+  vertices.forEach((v, i) => {
     if (abs(mouseX - v.x) < mouseSens && abs(mouseY - v.y) < mouseSens) {
-      console.log('ay');
+      // console.log('ay');
       grabbing_staged = true;
       grabbed_vtx = v;
+
     }
+
+    const next = (i+1) % 3;
+    const other = vertices[next];
+    const p = findMidpoint(v, other);
+    const m = findSlope(v, other);
+    orthos.push({m: m, p: p});
+    // drawLine(m, p);
+    // console.log(m, s);
   });
 }
 
@@ -60,10 +76,31 @@ function mouseDragged() {
   }
 }
 
-// Depends upon the global vertices array:
-function drawOrthLines() {
-
+function findMidpoint(p1, p2) {
+  return {
+    x: min([p1.x, p2.x]) + abs(p1.x - p2.x)/2,
+    y: min([p1.y, p2.y]) + abs(p1.y - p2.y)/2
+  };
 }
+
+function findSlope(p1, p2) {
+  return (p1.y - p2.y) / (p1.x - p2.x);
+}
+
+function drawLine(m, p) {
+  console.log(m, p);
+  const inv_slope = - 1 / m; // ignore the minus sign because of negative-y orientation with canvas.
+  // console.log(inv_slope);
+  const xStart = p.x - 600;
+  const yStart = p.y - 600 * inv_slope;
+  const xEnd = p.x + 600;
+  const yEnd = p.y + 600 * inv_slope;
+  stroke('green');
+  line(xStart, yStart, xEnd, yEnd);
+}
+
+
+
 
 
 // NOTE: Ok the UI isn't perfect, but it's kind of nice to be able to click a new spot and have most-recently-clicked vertex go there. I'm into it. (DON'T use the following function.)
