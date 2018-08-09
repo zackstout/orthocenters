c05e149f3d5e15451b3353e0f02a9f7df1e6172d
Zack Stout <zackstout@gmail.com>
2018-08-09 00:11:08 -0500

    this is a very interesting effect that draws ALL orth lines to the screen

--- scripts/draw.js
+++ scripts/draw.js
@@ -4,6 +4,7 @@ const mouseSens = 9;
 let grabbing_staged = false;
 let grabbing = false;
 let grabbed_vtx;
+const orthoLen = 800;
 let orthos = [];
 
 // expected mdpts: (200, 350), (400, 250), (500, 500) -- It works!
@@ -49,11 +50,11 @@ function mouseMoved() {
 
     }
 
-    const next = (i+1) % 3;
-    const other = vertices[next];
-    const p = findMidpoint(v, other);
-    const m = findSlope(v, other);
-    orthos.push({m: m, p: p});
+    // const next = (i+1) % 3;
+    // const other = vertices[next];
+    // const p = findMidpoint(v, other);
+    // const m = findSlope(v, other);
+    // orthos.push({m: m, p: p});
     // drawLine(m, p);
     // console.log(m, s);
   });
@@ -73,6 +74,14 @@ function mouseDragged() {
     grabbing = true; // not sure whether this is needed
     grabbed_vtx.x = mouseX;
     grabbed_vtx.y = mouseY;
+
+    vertices.forEach((v, i) => {
+      const next = (i+1) % 3;
+      const other = vertices[next];
+      const p = findMidpoint(v, other);
+      const m = findSlope(v, other);
+      orthos.push({m: m, p: p});
+    });
   }
 }
 
@@ -88,13 +97,13 @@ function findSlope(p1, p2) {
 }
 
 function drawLine(m, p) {
-  console.log(m, p);
+  // console.log(m, p);
   const inv_slope = - 1 / m; // ignore the minus sign because of negative-y orientation with canvas.
   // console.log(inv_slope);
-  const xStart = p.x - 600;
-  const yStart = p.y - 600 * inv_slope;
-  const xEnd = p.x + 600;
-  const yEnd = p.y + 600 * inv_slope;
+  const xStart = p.x - orthoLen;
+  const yStart = p.y - orthoLen * inv_slope;
+  const xEnd = p.x + orthoLen;
+  const yEnd = p.y + orthoLen * inv_slope;
   stroke('green');
   line(xStart, yStart, xEnd, yEnd);
 }
